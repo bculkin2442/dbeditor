085ff505353ae1d13d918a7d05dbdbf632a3dc3b
bculkin2442 <bjculkin@mix.wvu.edu>
2017-01-27 04:58:06 -0500

    Added SQL for templates

--- DBEditor/src/main/sql/db_schema.sql
+++ DBEditor/src/main/sql/db_schema.sql
@@ -104,6 +104,17 @@ create index Monsters_name on Monsters(name);
 create index Monsters_cr on Monsters(cr);
 create index Monsters_la on Monsters(la);
 
+create view Monsters_saves as 
+	select m.id, m.name, 
+	m.saves[1] as fortsave, m.saves[2] as refsave, m.saves[3] as willsave
+	from Monsters as m;
+
+create view Monsters_abilities as
+	select m.id, m.name,
+	m.abilityscores[1] as strength, m.abilityscores[2] as dexterity, m.abilityscores[3] as constitution,
+	m.abilityscores[4] as intelligence, m.abilityscores[5] as wisdom, m.abilityscores[6] as charisma
+	from Monsters as m;
+
 create table Monster_abilities (
 	monsterid int,
 	abilityid int,
@@ -134,3 +145,72 @@ create view Monsters_totalhd as
 	select m.id, m.name, sum(hd.count) as totalhd 
 	from Monsters as m join Monster_hitdice as hd on m.id = hd.monsterid
 	group by m.id, hd.monsterid;
+
+create view Monsters_playerdetails as
+	select m.id, m.name, m.la, m.class, hd.totalhd, (m.la + hd.totalhd) as effectivela
+	from Monsters as m join Monsters_totalhd as hd on m.id = hd.id;
+
+create table Templates (
+	id serial,
+	name varchar(255),
+	origin racetype array,
+	destinationtype racetype,
+	destinationsubtypes varchar(255) array,
+	hitdice varchar(255),
+	speed varchar(255),
+	saves int array,
+	abilityscores int array,
+	skilladj text,
+	featadj text,
+	climate text,
+	organization text,
+	cradj text,
+	la int,
+	lanotes text,
+	cohortonly boolean,
+	treasure text,
+	alignment text,
+	advancement text,
+	flavor text,
+	notes text,
+	source text,
+	primary key(id)
+);
+
+create index Templates_name on Templates(name);
+create index Templates_origin on Templates(origin);
+create index Templates_destination on Templates(destinationtype);
+create index Templates_origindest on Templates(origin, destinationtype);
+create index Templates_la on Templates(la);
+
+create table Template_abilities (
+	templateid int,
+	abilityid int,
+	isquality boolean,
+	primary key(templateid, abilityid),
+	foreign key(templateid) references Templates(id),
+	foreign key(abilityid) references Abilities(id)
+);
+
+create view Templates_abilityadj as
+	select t.id, t.name,
+	t.abilityscores[1] as strength, t.abilityscores[2] as dexterity, t.abilityscores[3] as constitution,
+	t.abilityscores[4] as intelligence, t.abilityscores[5] as wisdom, t.abilityscores[6] as charisma
+	from Templates as t;
+
+create view Templates_saveadj as 
+	select t.id, t.name, 
+	t.saves[1] as fortsave, t.saves[2] as refsave, t.saves[3] as willsave
+	from Templates as t;
+
+/* query to find templates with a certain destination type
+ * select t.id, t.name, t.la from Templates as t where ? = t.destinationtype order by t.la
+ * 
+ * query to find templates with a certain origin type
+ * select t.id, t.name, t.la from Templates as t where ? = any (t.origin) order by t.la
+ * 
+ * query to find templates from a origin to a destination
+ * select t.id, t.name, t.la from Templates as t where ? = any (t.origin) and ? = t.destinationtype order by la
+ */
+ 
+
